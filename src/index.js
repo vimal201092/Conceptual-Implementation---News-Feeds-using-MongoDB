bfa809a60258e9be4f1ea152ace5b6a5bd27f6c5
vimal201092 <vimalpal201092@gmail.com>
2020-12-21 19:41:35 +0530

    fin

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-const express = require('express')
+const express = require('express');
+
+//const { newsArticleSchema } = require('./schema');
 const app = express()
 const port = 8080
-
+const { newsArticleModel } = require('./connector');
+//const mongoosePaginate = require['mongoose-paginate'];
 const onePageArticleCount = 10
 
 
@@ -9,8 +12,17 @@ const onePageArticleCount = 10
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//newsArticleSchema.plugin(mongoosePaginate);
 
-
+app.get("/newFeeds", async (req, res) => {
+    res.send(await newsArticleModel.find().skip(sanitize(req.query.offset, 0)).limit(sanitize(req.query.limit, 10)));
+});
+    const sanitize = (value, defaultValue) => {
+        if (value === null || value === undefined || isNaN(Number(value))) {
+            return defaultValue;
+        }
+        return Number(value);    
+    }
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 module.exports = app;
